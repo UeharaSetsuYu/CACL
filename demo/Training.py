0c28db2be2f437c05c0bc331f26ee93e85fe6bfd
SetsuAyuGasaki <97835454+UeharaSetsuYu@users.noreply.github.com>
2026-05-12 14:07:41 +0800

    Update Training.py

--- demo/Training.py
+++ demo/Training.py
@@ -1,20 +1,21 @@
-import itertools 
+import itertools
 from tqdm import tqdm
-import torch  
+import torch
 import torch.optim as optim
-from torch.utils.data import TensorDataset
-import numpy as np 
-import collections 
-import random 
-from torch.optim.lr_scheduler import CosineAnnealingLR  
-
-from units.Clustering import *
+from torch.utils.data import Dataset, DataLoader, TensorDataset
+import numpy as np
+import collections
+import random
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.autograd import Function
+from units.datasets import load_data
+from units.unit import set_requires_grad, GRL_coeff, pseudo_clustering, get_supervised_metrics
 from model import *
 from units.loss import *
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED = True
 import warnings
-warnings.filterwarnings("ignore")  # 忽略所有警告
+warnings.filterwarnings("ignore")  # ignore all warnings
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
@@ -39,8 +40,8 @@ def reconstruction(model, data_list, view_num, Criterion, optimizer, target_list
     loss = sum(loss_list)  # Reconstruction Loss only, now
     loss_1 += loss.item() / view_num
  
-    if Flag:
-        visualize_raw_images_grid(X[0], X[1], target_list[0], mask, 5)
+    # if Flag:
+    #     visualize_raw_images_grid(X[0], X[1], target_list[0], mask, 5)
 
 
     loss.backward()
@@ -70,6 +71,46 @@ def UCRM(model, data_list, view_num, Criterion, optimizer):
 
     return loss_3
 
+
+def Classification(model, data_list, view_num, Criterion, optimizer, y):
+    loss_list = []
+    loss_list_cla = []
+    y = list(y)
+    for i in range(view_num): y[i] = y[i].to(data_list[i].device).long()
+    *_, pseudo_list, _, _ = model(data_list)
+    loss_3 = 0
+    optimizer.zero_grad()
+    CE_loss = nn.CrossEntropyLoss(label_smoothing=0.1)
+    # CE_loss = nn.CrossEntropyLoss()
+    # CE_loss = Criterion.forward_
+    for i in range(view_num):
+
+        #         q_v = pseudo_list[i]
+
+        #         loss_list_cla.append(CE_loss(q_v, y[i]))
+        # loss_list_cla.append(Criterion.forward_Focal_CE(q_v, y[i]))
+
+        for j in range(i + 1, view_num):
+            q_m = pseudo_list[i]
+            q_n = pseudo_list[j]
+            loss_align = Criterion.pseudoAlignLoss(q_m, q_n)
+
+            loss_list.append(loss_align)
+    loss_1 = 0.5 * (sum(loss_list)) / view_num
+
+    for i in range(view_num):
+        q_v = pseudo_list[i]
+        # loss_list_cla.append(CE_loss(q_v, y[i]))
+        loss_list_cla.append(Criterion.forward_Focal_CE(q_v, y[i]))
+
+    loss_2 = sum(loss_list_cla) / view_num
+
+    loss = loss_1 * 0.1 + loss_2
+    loss_3 += loss.item()
+
+    loss.backward()
+    optimizer.step()
+    return loss_3
 def RecUCRM(model, data_list, view_num, Criterion, optimizer):
     x_hat, *_ = model(
         data_list)  # type is list overall
@@ -158,7 +199,44 @@ def GRCAM_Bi(model, data_list, view_num, Criterion, args, device, optimizer_CRMN
 
     return loss_1, loss_2
 
+def GRCAM_Bi_woAdv(model, data_list, view_num, Criterion, optimizer_CRMN):
+    '''
+        *_, z_share = model(data_list) , z_share is consistency representations, type is list
+    '''
 
+    *_, z_share = model(data_list)
+
+    '''Training CRMN'''
+    optimizer_CRMN.zero_grad()
+    model.MLP.train()
+
+    loss_list_con = []
+
+
+    for i in range(view_num):
+        for j in range(i + 1, view_num):
+            z_1 = z_share[i]
+            z_2 = z_share[j]
+
+            # MI based
+            # loss_view = Criterion.forward_MI(z_1, z_2)
+
+            # MSE based
+            loss_view = Criterion.mse(z_1, z_2)
+
+            loss_list_con.append(loss_view)
+
+
+
+
+
+
+    loss_consistency = sum(loss_list_con) / view_num
+    loss_1 = loss_consistency.item()
+    loss_consistency.backward()
+    optimizer_CRMN.step()
+
+    return loss_1
 
 def CRFN_Adversarial(model, data_list, view_num, Criterion, args, device, optimizer_CRFN, optimizer_Disc):
     loss_list_disc = []
@@ -206,6 +284,56 @@ def CRFN_Adversarial(model, data_list, view_num, Criterion, args, device, optimi
         loss_filter = aplha * Criterion.forward_CrossEntropy(specific_logit, specific_label) + (
                 1 - aplha) * Criterion.forward_CrossEntropy(shared_logit, share_label) + \
                   eta * Criterion.forward_Entropy(specific_logit)
+        loss_list_crfn.append(loss_filter)
+    loss_filter = sum(loss_list_crfn)
+    loss_2 += loss_filter.item() / view_num
+    loss_filter.backward()
+    optimizer_CRFN.step()
+
+    return loss_1, loss_2
+
+
+def CRFN_Adversarial_NoneConsis(model, data_list, view_num, Criterion, args, device, optimizer_CRFN, optimizer_Disc):
+    loss_list_disc = []
+
+    loss_1, loss_2 = 0, 0
+    '''Training Discriminator'''
+    set_requires_grad(model.discriminator, True)
+    set_requires_grad(model.filter, False)
+    model.filter.eval()
+    model.discriminator.train()
+    _, latent_share, latent_specific, hlz, z_con, *_, z_share = model(data_list)
+    optimizer_Disc.zero_grad()
+
+    aplha = args.epsilon
+    eta = 10
+    for view in range(view_num):
+        specific_label = torch.full((z_share[0].shape[0],), view , dtype=torch.long, device=device)
+        specific_logit = model.discriminator(z_con[view].detach())
+        shared_logit = model.discriminator(z_share[view].detach())
+        loss_ce = aplha * Criterion.forward_CrossEntropy(specific_logit, specific_label) + eta * Criterion.forward_Entropy(specific_logit)
+        loss_list_disc.append(loss_ce)
+
+    loss_disc = sum(loss_list_disc)
+
+    loss_1 += loss_disc.item() / view_num
+    loss_disc.backward()
+    optimizer_Disc.step()
+
+
+    '''Training Filter'''
+    set_requires_grad(model.discriminator, False)
+    set_requires_grad(model.filter, True)
+    model.filter.train()
+    model.discriminator.eval()
+    _, latent_share, latent_specific, hlz, z_con, *_, z_share = model(data_list)
+    optimizer_CRFN.zero_grad()
+    loss_list_crfn = []
+
+    for view in range(view_num):
+        specific_label = torch.full((z_share[0].shape[0],), view , dtype=torch.long, device=device)
+        specific_logit = model.discriminator(z_con[view])
+        loss_filter = aplha * Criterion.forward_CrossEntropy(specific_logit, specific_label) + eta * Criterion.forward_Entropy(specific_logit)
         loss_list_crfn.append(loss_filter)
     loss_filter = sum(loss_list_crfn)
     loss_2 += loss_filter.item() / view_num
@@ -288,9 +416,7 @@ def main_train(args, dataset_name, config, device):
             data_list, target_list = zip(*batch)
             data_list = list(data_list)
 
-            if flag:
-                visualize_raw_images_grid(data_list[0], data_list[1], target_list[0], 5)
-                flag = False
+
 
             for view in range(view_num):
                 data_list[view] = data_list[view].to(device)
@@ -303,26 +429,34 @@ def main_train(args, dataset_name, config, device):
             if epoch < pre_train:
 
                 loss_1, loss_2 = GRCAM_Bi(model, data_list, view_num, Criterion, args, device, optimizer_CRMN, optimizer_Cla, iter_idx, num_iters)
- 
+
                 iter_idx += 1
                 loss_adver_cla += loss_1
                 loss_consis += loss_2
+
+                # loss_1 = GRCAM_Bi_woAdv(model, data_list, view_num, Criterion, optimizer_CRMN)
+                # loss_adver_cla += 0
+                # loss_consis = 0
 
             else:
                 set_requires_grad(model.classifier, False)
                 set_requires_grad(model.MLP, False)
                 model.classifier.eval()
+                # set_requires_grad(model.Encoder_c, False)
                 model.MLP.eval()
                 #
                 #
-                loss_1, loss_2 = CRFN_Adversarial(model, data_list, view_num, Criterion, args, device, optimizer_CRFN, optimizer_Disc)
+                loss_1, loss_2 = CRFN_Adversarial_NoneConsis(model, data_list, view_num, Criterion, args, device, optimizer_CRFN, optimizer_Disc)
      
                 loss_adver_disc += loss_1
                 loss_comple += loss_2
  
 
-                loss_rec += reconstruction(model, data_list, view_num, Criterion, optimizer_Rec, target_list, Flag, mask)
-                loss_Clu += UCRM(model, data_list, view_num, Criterion, optimizer_UCM)
+                loss_rec += reconstruction(model, data_list, view_num, Criterion, optimizer_Rec, target_list, Flag)
+                if args.model == 'Clustering':
+                    loss_Clu += UCRM(model, data_list, view_num, Criterion, optimizer_UCM)
+                elif args.model == 'Classification':
+                    loss_Clu += Classification(model, data_list, view_num, Criterion, optimizer_UCM, target_list)
 
             all_loss += loss_consis + loss_adver_cla + loss_adver_disc + loss_rec + loss_Clu + loss_adver_cla + loss_comple
             loop.set_postfix(Recon_Loss=f"{loss_rec: .6f}",
@@ -332,10 +466,13 @@ def main_train(args, dataset_name, config, device):
                              zDiscri_Loss = f"{loss_adver_disc: .6f}",
                              zClassifer_Loss = f"{loss_adver_cla: .6f}",
                              Pseudo_Loss = f"{loss_Clu: .6f}")
-
-            loss_metrics['Consistency_Loss'].append(loss_consis)
-            loss_metrics['Discriminator'].append(loss_adver_disc)
-            loss_metrics['Classifier_Loss'].append(loss_adver_cla)
+            '''Stage one Loss'''
+            loss_metrics['Consistency_Loss'].append(loss_consis)    # Adversarial Loss by ConRE
+            loss_metrics['Classifier_Loss'].append(loss_adver_cla)  # Adversarial Loss by Discriminator
+            '''Stage two Loss'''
+            # loss_metrics['Discriminator'].append(loss_adver_disc)   # Classifier Loss
+            loss_metrics['Complementarity'].append(loss_adver_disc + loss_comple + loss_Clu + loss_rec)
+            '''All Loss'''
             loss_metrics['all_loss'].append(all_loss)
 
 
@@ -345,23 +482,84 @@ def main_train(args, dataset_name, config, device):
         scheduler_Cla.step()
         shecduler_RecUCM.step()
 
-        if epoch >= pre_train:
+        if (epoch + 1) % 10 == 0 and epoch > args.pre_train:
             Evaluation(model, test_loader, device, accumulated_metrics, view_num, bestmodel_metrics, epoch)
 
-
-
             if (epoch + 1) % 10 == 0:
-                print(f'Best clustering performance: acc: {accumulated_metrics["acc"][-1]}, '
-                      f'ari: {accumulated_metrics["ari"][-1]}, nmi: {accumulated_metrics["nmi"][-1]}, '
-                      f'pur: {accumulated_metrics["pur"][-1]:.4f}, f_mea: {accumulated_metrics["f_mea"][-1]:.4f}')
+                if args.model == 'Classification':
+                    Evaluation_classifcation(model, test_loader, device, accumulated_metrics, view_num,
+                                             bestmodel_metrics, epoch)
+                    print(f'Best clustering performance: acc: {accumulated_metrics["acc"][-1]}, '
+                          f'precision: {accumulated_metrics["precision"][-1]:.4f}, f_mea: {accumulated_metrics["f_mea"][-1]:.4f}')
+                elif args.model == 'Clustering':
+                    Evaluation(model, test_loader, device, accumulated_metrics, view_num, bestmodel_metrics, epoch)
+                    print(f'Best clustering performance: acc: {accumulated_metrics["acc"][-1]}, '
+                          f'ari: {accumulated_metrics["ari"][-1]}, nmi: {accumulated_metrics["nmi"][-1]}, '
+                          f'pur: {accumulated_metrics["pur"][-1]:.4f}, f_mea: {accumulated_metrics["f_mea"][-1]:.4f}')
 
-    print(f'Best clustering performance: acc: {accumulated_metrics["bestacc"]}, '
-          f'ari: {accumulated_metrics["bestari"]}, nmi: {accumulated_metrics["bestnmi"]}, '
-          f'sc: {accumulated_metrics["bestsc"]:.4f}, pur: {accumulated_metrics["bestpur"]:.4f}, f_mea: {accumulated_metrics["bestfmea"]:.4f}')
+    if args.model == 'Classification':
+        print(f'Best clustering performance: acc: {accumulated_metrics["bestacc"]}, ' 
+              f'precision: {accumulated_metrics["best_precision"]}, f_mea: {accumulated_metrics["bestfmea"]}')
+    elif args.model == 'Clustering':
+        print(f'Best clustering performance: acc: {accumulated_metrics["bestacc"]}, '
+              f'ari: {accumulated_metrics["bestari"]}, nmi: {accumulated_metrics["bestnmi"]}, '
+              f'pur: {accumulated_metrics["bestpur"]:.4f}')
+
+    # import time
+    # torch.save(bestmodel_metrics['bestModel'],
+    #            './AAA_Bestmodel_Ablation/' + dataset_name + '_' + 'ACC-' + str(accumulated_metrics["bestacc"]) + time.strftime(
+    #                "_%Y%m%d-%H%M%S") + '.pth')
+
+
+    # print(f'Best clustering performance: acc: {accumulated_metrics["bestacc"]}, '
+    #       f'ari: {accumulated_metrics["bestari"]}, nmi: {accumulated_metrics["bestnmi"]}, '
+    #       f'sc: {accumulated_metrics["bestsc"]:.4f}, pur: {accumulated_metrics["bestpur"]:.4f}, f_mea: {accumulated_metrics["bestfmea"]:.4f}')
 
     return accumulated_metrics, loss_metrics
 
+def Evaluation_classifcation(model, test_loader, device, accumulated_metrics, view_num, bestmodel_metrics, epoch):
 
+    all_labels_true, all_labels_pred = [], []
+    z_feature, feature, origin_data = [], [], []
+    with torch.no_grad():  # if you want bar, you can copy total=len(train_loader[0]) in loop,
+        model.eval()
+        data = enumerate(zip(*test_loader))
+        for batch_idx, batch in data:
+            data_list, target_list = zip(*batch)
+            data_list = list(data_list)
+            for i in range(view_num): data_list[i] = data_list[i].to(device)
+            _, latent_share, latent_specific, hlz, z_con, specific_prob, share_prob, pseudo_list, _, z_shared = model(data_list)
+
+            '''Pseudo-Clustering Result'''
+            p = torch.stack(pseudo_list)
+            max_probs = torch.mean(p, dim=0)
+            final_p, final_c = torch.max(max_probs, dim = 1)
+            y = final_c.detach().cpu().numpy().ravel().astype(int)
+            label = target_list[0].numpy().ravel()
+
+            '''Clustering Result Recording'''
+            all_labels_true.extend(label)
+            all_labels_pred.extend(y)
+
+        # pseudo_clustering([], all_labels_pred, all_labels_true, accumulated_metrics['acc'],
+        #                   accumulated_metrics['ari'],
+        #                   accumulated_metrics['nmi'],
+        #                   accumulated_metrics['precision'], accumulated_metrics['pur'], accumulated_metrics['f_mea'])
+        acc, pre, f1 = get_supervised_metrics(all_labels_true, all_labels_pred)
+        accumulated_metrics['acc'].append(acc)
+        accumulated_metrics['precision'].append(pre)
+        accumulated_metrics['f_mea'].append(f1)
+
+        if epoch >= 0:
+                if (accumulated_metrics['bestacc'] < acc):
+                    accumulated_metrics['bestacc'] = acc
+
+                    accumulated_metrics['best_precision'] = pre
+
+                    accumulated_metrics['bestfmea'] = f1
+                    bestModel = model.state_dict()
+
+                    bestmodel_metrics['bestModel'] = bestModel
 def Evaluation(model, test_loader, device, accumulated_metrics, view_num, bestmodel_metrics, epoch):
 
     all_labels_true, all_labels_pred = [], []
@@ -375,7 +573,7 @@ def Evaluation(model, test_loader, device, accumulated_metrics, view_num, bestmo
             for i in range(view_num): data_list[i] = data_list[i].to(device)
             _, latent_share, latent_specific, hlz, z_con, specific_prob, share_prob, pseudo_list, _, z_shared = model(data_list)
 
-            im.append(compute_mmd(z_shared, z_con, view_num))
+            # im.append(compute_mmd(z_shared, z_con, view_num))
 
 
 
@@ -450,7 +648,8 @@ def data_preprocess(dataset_name, args) ->list:
         y_test_tensor = torch.tensor(y_test, dtype=torch.int32)
         dataset_train = TensorDataset(x_train_tensor, y_train_tensor)
         dataset_test = TensorDataset(x_test_tensor, y_test_tensor)
-        data_train_loader = DataLoader(dataset_train, batch_size=args.batch_size, shuffle=False, drop_last=True)    # 已经打乱数据了不需要再次打乱(shuffle = False)
+         # The data has already been shuffled and does not need to be shuffled again. (shuffle = False)
+        data_train_loader = DataLoader(dataset_train, batch_size=args.batch_size, shuffle=False, drop_last=True)   
         data_test_loader = DataLoader(dataset_test, batch_size=args.batch_size, shuffle=False, drop_last=False)
         train_loader.append(data_train_loader)
         test_loader.append(data_test_loader)
