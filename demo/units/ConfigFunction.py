bfaeb4b9d92df45f4a31dc032d83e4d8b1ff80c4
SetsuAyuGasaki <97835454+UeharaSetsuYu@users.noreply.github.com>
2026-05-24 18:16:50 +0800

    Update ConfigFunction.py

--- demo/units/ConfigFunction.py
+++ demo/units/ConfigFunction.py
@@ -10,7 +10,8 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 def load_data(dataset_name):
     args = parse_args()
     view_num = args.view_num
-      
+    X_list = []
+    label = 0
     if dataset_name == 'BDGP':
         '''
             This is drosophila embryos, each of which is represented by visual and textual features.
@@ -21,25 +22,41 @@ def load_data(dataset_name):
         X = [normalize(mat['X1'].astype(np.float32), flag='row_vector'),
              normalize(mat['X2'].astype(np.float32), flag='row_vector')]
         y = np.squeeze(mat['Y']).astype('int')
-        X_list = []
+        
         for view in range(2):
             X_list.append(X[view])
         if np.min(y) == 1:
             y = y - 1
-        return X_list, y 
+            
+        label = y
 
     elif dataset_name == 'NGs':
         '''
             contains 500 instance in 5 clusters for 3 vies and feature dimension is [2000, 2000, 2000]
         '''
-        mat = sio.loadmat('./data/NGs.mat')
-        X_list = []
+        mat = sio.loadmat('./data/NGs.mat') 
         for view in range(3):
             X = mat['X']
             X_list.append(normalize(X[view][0], flag='row_vector'))
         y = np.squeeze(mat['Y']).astype('int')
         if np.min(y) == 1:
             y = y - 1
-        return X_list, y
+        label = y
 
-     
+    elif dataset_name == 'BBCSport':
+        '''
+             BBCSportL: Contains 544 samples in 5 clusters for 2 views  3183/3203, 
+              ''' 
+        mat = sio.loadmat('./data/BBCSport/BBCSport')
+        for i in range(view_num):
+            x = mat['X'][0][i].T
+            x = x.toarray() 
+            X_list.append(normalize(x, flag='row_vector'))
+
+        y = np.squeeze(mat['Y']).astype('int')
+        if np.min(y) == 1:
+            y = y - 1
+        label = y
+
+
+return X_list, label 
