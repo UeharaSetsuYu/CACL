9d37c92b527f31d691685afd94b5ad4faec2cb63
SetsuAyuGasaki <97835454+UeharaSetsuYu@users.noreply.github.com>
2026-05-24 18:18:26 +0800

    Update datasets.py

--- demo/units/datasets.py
+++ demo/units/datasets.py
@@ -10,111 +10,53 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 def load_data(dataset_name):
     args = parse_args()
     view_num = args.view_num
-      
+    X_list = []
+    label = 0
     if dataset_name == 'BDGP':
         '''
             This is drosophila embryos, each of which is represented by visual and textual features.
             view 1 and 2 is represented by visual and textual features.
             view_num = 2, classes = 5, samples = 2500
         '''
-        view_num = 2
-        mat = sio.loadmat('./data/BDGP.mat')
+        mat = sio.loadmat('D:\Data_Mining\Code\Datasets\BDGP\BDGP.mat')
         X = [normalize(mat['X1'].astype(np.float32), flag='row_vector'),
              normalize(mat['X2'].astype(np.float32), flag='row_vector')]
         y = np.squeeze(mat['Y']).astype('int')
-        X_list = []
-        for view in range(view_num):
+        
+        for view in range(2):
             X_list.append(X[view])
         if np.min(y) == 1:
             y = y - 1
-        return X_list, y
-
-
-    elif dataset_name == 'BBCSport':
-        '''
-             BBCSportL: Contains 544 samples in 5 clusters for 2 views  3183/3203, 
-              '''
-        X_list = []
-        mat = sio.loadmat('./data/BBCSport/BBCSport')
-        for i in range(view_num):
-            x = mat['X'][0][i].T
-            x = x.toarray()
-
-            # print(x.shape)
-            # x = normalize(mat['X'][0][i].T, flag="row_vector")
-            X_list.append(normalize(x, flag='row_vector'))
-
-        y = np.squeeze(mat['Y']).astype('int')
-        if np.min(y) == 1:
-            y = y - 1
-        return X_list, y
+            
+        label = y
 
     elif dataset_name == 'NGs':
         '''
             contains 500 instance in 5 clusters for 3 vies and feature dimension is [2000, 2000, 2000]
         '''
-        mat = sio.loadmat('./data/NGs.mat')
-        X_list = []
+        mat = sio.loadmat('./data/NGs.mat') 
         for view in range(3):
             X = mat['X']
             X_list.append(normalize(X[view][0], flag='row_vector'))
         y = np.squeeze(mat['Y']).astype('int')
         if np.min(y) == 1:
             y = y - 1
-        return X_list, y
+        label = y
 
-    elif dataset_name == 'Hdigit':
+    elif dataset_name == 'BBCSport':
         '''
-            contains 10000 instance in 10 clusters for 2 views and feature dimension is  [784, 256]
-        '''
-        X_list = []
-        mat = sio.loadmat('./data/Hdigit.mat')
-        X = mat['data'][0]
-        for view in range(view_num):
-            X_list.append(normalize(X[view].T))
-        y = np.squeeze(mat['truelabel'][0][0]).astype('int')
-        if np.min(y) == 1:
-            y -= 1
-        return X_list, y
-    elif dataset_name == 'cifar10':
-        '''contains 50000 instance in 10 clusters for 3 views and feature dimension is  [512, 2048, 1024]'''
-        X_list = []
-        mat = sio.loadmat('./data/cifar10.mat')
-        X = mat['data']
-        for view in range(view_num):  # view_num = 3
-            X_list.append(normalize(X[view][0].T, flag='row_vector'))  # sigma, row
-        y = np.squeeze(mat['truelabel'][0][0]).astype('int')
-        if np.min(y) == 1:
-            y -= 1
-        return X_list, y
-
-    elif dataset_name == 'cora':
-        '''contains 2708 instance in 7 clusters for 2 views and feature dimension is  [2708, 1433]'''
-        X_list = []
-        mat = sio.loadmat('./data/Cora.mat')
-        X1 = mat['coracites']
-        X2 = mat['coracontent']
-        for i in [X1, X2]:
-            X_list.append(normalize(i, 'row_vector'))
-        y = np.squeeze(mat['y']).astype('int')
-        if np.min(y) == 1:
-            y -= 1
-        return X_list, y
-
-    elif dataset_name == 'Movies':
-        '''
-            contains 617 instance in 17 clusters for 2 views and feature dimension is [1878, 1398]
-            It is a movie corpus extracted from IMDb. X. Xie and Y. Xiong, “Generalized multi-view learning based on generalized eigenvalues proximal support vector machines,” Expert Systems with Applications, vol. 194, p. 116491, May 2022, doi: 10.1016/j.eswa.2021.116491.
-        '''
-        X_list = []
-        mat = sio.loadmat('./data/Movies.mat')
-        X = mat['X']
+             BBCSportL: Contains 544 samples in 5 clusters for 2 views  3183/3203, 
+              ''' 
+        mat = sio.loadmat('./data/BBCSport/BBCSport')
         for i in range(view_num):
-            X_list.append(normalize(X[i][0], flag='row_vector'))
-            print(X[i][0].shape)
+            x = mat['X'][0][i].T
+            x = x.toarray() 
+            X_list.append(normalize(x, flag='row_vector'))
 
-        y = np.squeeze(mat['y']).astype('int')
+        y = np.squeeze(mat['Y']).astype('int')
         if np.min(y) == 1:
-            y -= 1
+            y = y - 1
+        label = y
 
-        return X_list, y
+
+return X_list, label 
